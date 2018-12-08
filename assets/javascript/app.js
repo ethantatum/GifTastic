a7a79d7915da4594202f47fa8056046ce01f7429
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-12-08 09:53:43 -0700

    changed theme, added replace to allow for multiple word searches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,80 +2,82 @@ $(document).ready(function() {
     // VARIABLES
     // ============================================================================
 
-    // Initial array of bands
-    var bands = ["Metallica", "Green Day", "Van Halen", "OK Go"];
+    // Initial array of cartoons
+    var cartoons = ["ThunderCats", "He-Man", "G.I. Joe", "She-Ra", "DuckTales", "Inspector Gadget"];
 
 
     // FUNCTIONS
     // ============================================================================
 
-    // displayBandGif function re-renders the HTML to display the appropriate content
-    function displayBandGif() {
+    // displayToonGif function re-renders the HTML to display the appropriate content
+    function displayToonGif() {
 
-        let band = $(this).attr("data-name");
-        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${band}&api_key=TqXsiW057A6TfMyJpYRpF4IynG9rHxk5&limit=10`;
+        let toon = $(this)
+                .attr("data-name")
+                .replace(' ', '+');
+        let queryURL = `https://api.giphy.com/v1/gifs/search?q=${toon}&api_key=TqXsiW057A6TfMyJpYRpF4IynG9rHxk5&limit=10`;
 
-        // Creates AJAX call for the specific band button being clicked
+        // Creates AJAX call for the specific toon button being clicked
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response) {
             console.log(response);
 
-            // Creates a div to hold the band
-            //$(`#bands-view`).append(`Title: ${response.Title}<br>`);
+            // Creates a div to hold the toon
+            //$(`#toons-view`).append(`Title: ${response.Title}<br>`);
             // Retrieves the Rating Data
             // Creates an element to have the rating displayed
             // Displays the rating
-            //$(`#bands-view`).append(`Rating: ${response.Rated}<br>`);
+            //$(`#toons-view`).append(`Rating: ${response.Rated}<br>`);
             // Retrieves the release year
             // Creates an element to hold the release year
             // Displays the release year
-            //$(`#bands-view`).append(`Release Year: ${response.Released}<br>`);
+            //$(`#toons-view`).append(`Release Year: ${response.Released}<br>`);
             // Retrieves the plot
             // Creates an element to hold the plot
             // Appends the plot
-            //$(`#bands-view`).append(`Plot: ${response.Plot}<br>`);
+            //$(`#toons-view`).append(`Plot: ${response.Plot}<br>`);
             // Creates an element to hold the image
             // Appends the image
-            //$(`#bands-view`).append(`<img src=${response.Poster}><br>`);
-            // Puts the entire band above the previous bands.
+            //$(`#toons-view`).append(`<img src=${response.Poster}><br>`);
+            // Puts the entire toon above the previous toons.
         });
 
     }
 
-    // Function for displaying band data
+    // Function for displaying toon data
     function renderButtons() {
 
-        // Delete the content inside the bands-view div prior to adding new bands
-        $(`#bands-view`).empty();
+        // Delete the content inside the toons-view div prior to adding new toons
+        $(`#toons-view`).empty();
 
-        //Loop through the array of bands, then generate buttons for each band in the array
-        for(let i = 0; i < bands.length; i++) {
-        $(`#bands-view`)
-                .append(`<button>${bands[i]}</button>`)
+        //Loop through the array of toons, then generate buttons for each toon in the array
+        for(let i = 0; i < cartoons.length; i++) {
+        $(`#toons-view`)
+                .append(`<button>${cartoons[i]}</button>`)
                 .addClass("band");
         }
 
     }
 
-      // This function handles events where the add band button is clicked
-      $("#add-band").on("click", function(event) {
+      // This function handles events where the add toon button is clicked
+      $("#add-toon").on("click", function(event) {
         event.preventDefault();
         // This line of code will grab the input from the textbox
-        let band = $("#movie-input").val().trim();
+        let toon = $("#toon-input").val().trim();
 
-        // The band from the textbox is then added to our array
-        bands.push(band);
+        // The toon from the textbox is then added to our array
+        cartoons.push(toon);
 
-        // Calling renderButtons which handles the processing of our movie array
+        // Calling renderButtons which handles the processing of our cartoons array
         renderButtons();
       });
 
-      // Adding click event listeners to all elements with a class of "band"
-      $(document).on("click", ".band", displayBandGif);
+      // Adding click event listeners to all elements with a class of "toon"
+      $(document).on("click", ".toon", displayToonGif);
 
-      // Calling the renderButtons function to display the initial list of bands
+      // Calling the renderButtons function to display the initial list of cartoons
       renderButtons();
 
 
